6911ec79a41ee494b7600544cdd8c19b135fe96a
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-04-23 19:16:44 -0400

    implement die

--- die.ps1
+++ die.ps1
@@ -1,1 +1,16 @@
+#!/usr/bin/env pwsh
 
+function die {
+    param (
+        [alias('m')][string]$diemsg,
+        [alias('c')][int]$exitcode = 0
+    )
+    if ($diemsg) {
+        if ($host.UI.SupportsVirtualTerminal) { 
+            write-output ("{0}[1;38;2;355;0;0m{1}{0}[0m" -f ([char]0x1B),$diemsg)
+        } else {
+            write-output $diemsg
+        }
+    }
+    exit $exitcode
+}
